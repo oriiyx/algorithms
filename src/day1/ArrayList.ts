b4f6f083a38a84ec245508dc2366a8735c7dda4d
Peter Paravinja <peter.paravinja@optiweb.com>
2024-08-13 15:13:08 +0200

    Array List

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,31 +1,51 @@
 export default class ArrayList<T> {
-    public length: number;
-
+    private readonly items: T[]; // Underlying array to store elements
+    public length: number; // Number of elements in the ArrayList
 
     constructor() {
+        this.items = [];
+        this.length = 0;
     }
 
     prepend(item: T): void {
-
+        this.items.unshift(item);
+        this.length++;
     }
 
     insertAt(item: T, idx: number): void {
-
+        if (idx < 0 || idx > this.length) {
+            throw new RangeError("Index out of bounds");
+        }
+        this.items.splice(idx, 0, item);
+        this.length++;
     }
 
     append(item: T): void {
-
+        this.items.push(item);
+        this.length++;
     }
 
     remove(item: T): T | undefined {
-
+        const idx = this.items.indexOf(item);
+        if (idx === -1) {
+            return undefined;
+        }
+        return this.removeAt(idx);
     }
 
     get(idx: number): T | undefined {
-
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
+        return this.items[idx];
     }
 
     removeAt(idx: number): T | undefined {
-
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
+        const removedItem = this.items.splice(idx, 1)[0];
+        this.length--;
+        return removedItem;
     }
 }
